5e55f2d3682d70d6a5a48aebc0d926024eacda3b
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-02-17 00:54:33 -0500

    Added material buttons

--- src/components/itemModal.tsx
+++ src/components/itemModal.tsx
@@ -39,11 +39,11 @@ const ItemModal: FC<ItemModalProps> = ({ setModalOpen, setItems, currentShopping
               </p>
 
               <div className="mb-3 pt-0">
-                <Input variant="outlined" value={nameInput} onChange={(e) => setNameInput(e.target.value)} label="Item name" className="px-2 py-3 placeholder-slate-500 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"/>
+                <Input variant="outlined" onChange={(e) => setNameInput(e.target.value)} label="Item name" className="px-2 py-3 placeholder-slate-500 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"/>
               </div>
 
               <div className="mb-3 pt-0">
-                <Input variant="outlined" value={quantityInput} onChange={(e) => setQuantityInput(parseInt(e.target.value))} label="Item quantity" className="px-2 py-3 placeholder-slate-500 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"/>
+                <Input variant="outlined" onChange={(e) => setQuantityInput(parseInt(e.target.value))} label="Item quantity" className="px-2 py-3 placeholder-slate-500 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"/>
               </div>
             </div>
             {/*footer*/}
